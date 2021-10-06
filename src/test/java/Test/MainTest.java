d7f25e156d087d11b95dbab5f7465bb6e8102012
Antony <antony@mail.com>
2021-10-06 19:54:45 +0300

    added login to Admin Panel

--- src/test/java/Test/MainTest.java
+++ src/test/java/Test/MainTest.java
@@ -3,24 +3,30 @@ package test.java.Test;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.fail;
 
 public class MainTest extends BaseMainTest {
 
-    @Test(description = "check HomePage Title")
-    @Severity(SeverityLevel.NORMAL)
-    public void checkHomePageTitle() {
-        homePage.isShown();
+    @BeforeMethod(description = "login to Admin Panel")
+    public void logInToAdmin() {
+        adminPanel.logIn();
+    }
 
-        Assert.assertEquals(driver.getTitle(), "Google");
+    @Test(description = "check AdminPanel Title")
+    @Severity(SeverityLevel.NORMAL)
+    public void checkAdminPanelTitle() {
+        adminPanel.isShown();
+
+        Assert.assertEquals(adminPanel.getTitle(), "My Store");
     }
 
     @Test(description = "debug Test for fail result")
     @Severity(SeverityLevel.TRIVIAL)
     public void testFail() {
-        homePage.isShown();
+        adminPanel.isShown();
         fail();
     }
 }
